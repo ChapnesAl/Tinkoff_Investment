65e9f47dee4798d5ad8769d01a3cb4f405c2ddad
alexeychernysh <job.chap@icloud.com>
2022-08-01 12:18:27 +0300

    7 test - with new column gf_copy.iloc[ind[i], 3] < 5: 907

--- get_tables/table_str3.py
+++ get_tables/table_str3.py
@@ -104,7 +104,7 @@ def ts3(market_ticker, stock_ticker):  # table from strategy 1
                         and gf_copy.iloc[ind[i] - 3, 4] > 0 and gf_copy.iloc[ind[i] - 3, 4] < 25 \
                         and gf_copy.iloc[ind[i] - 4, 4] > 0 and gf_copy.iloc[ind[i] - 4, 4] < 20 \
                         and gf_copy.iloc[ind[i] - 5, 4] < 20 \
-                        and gf_copy.iloc[ind[i], 3] < 10:
+                        and gf_copy.iloc[ind[i], 3] < 5:
 
 
 

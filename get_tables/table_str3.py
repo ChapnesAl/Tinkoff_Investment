b33032a824d09cff58c627031c14a2e116033423
alexeychernysh <job.chap@icloud.com>
2022-08-01 15:40:47 +0300

    19 test - with new column gf_copy.iloc[ind[i]-1, 3] > -25: 928

--- get_tables/table_str3.py
+++ get_tables/table_str3.py
@@ -105,10 +105,6 @@ def ts3(market_ticker, stock_ticker):  # table from strategy 1
                         and gf_copy.iloc[ind[i] - 4, 4] > 0 and gf_copy.iloc[ind[i] - 4, 4] < 20 \
                         and gf_copy.iloc[ind[i] - 5, 4] < 20 \
                         and gf_copy.iloc[ind[i], 3] < 4:
-
-
-
-
                     r[i] = 'Short'  # buy
                     # r[i] = 1  # buy
                 elif gf_copy.iloc[ind[i], 4] < 0 and gf_copy.iloc[ind[i], 4] > -2 \
@@ -117,7 +113,7 @@ def ts3(market_ticker, stock_ticker):  # table from strategy 1
                         and gf_copy.iloc[ind[i] - 3, 4] < -1 and gf_copy.iloc[ind[i] - 3, 4] > -30 \
                         and gf_copy.iloc[ind[i] - 4, 4] < 10 \
                         and gf_copy.iloc[ind[i], 3] > -10\
-                        and gf_copy.iloc[ind[i]-1, 3] > -30:
+                        and gf_copy.iloc[ind[i]-1, 3] > -25:
 
                     # and gf_copy.iloc[ind[i] - 5, 4] < 20:
                     r[i] = 'Long'  # sell

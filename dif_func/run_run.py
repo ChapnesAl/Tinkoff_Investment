215e39a5ac2a6ad7b2d7eec6fb3d8dbb6ec5708e
alexeychernysh <job.chap@icloud.com>
2022-08-03 12:24:12 +0300

    add more argument-func

--- dif_func/run_run.py
+++ dif_func/run_run.py
@@ -4,6 +4,11 @@ from paper_filter.lists_papers import val_all_usd, val_long_usd, val_long_rub, v
 mt = '^GSPC'
 st = 'T'
 
-print(Str3(mt, val_all_rub).sum_results_rus())
 
+# print(Str3('^GSPC', 'T').signals())
+print(Str3(mt, 'T').sum_results())
+# print(Str3('^GSPC', 'T').get_table())
+# print(Str3('^GSPC', 'T').month_results())
+# print(Str3('^GSPC', ['T', 'AA']).month_results_without_bad())
+# print(Str3('^GSPC', val_all_rub).sum_results_rus())
 

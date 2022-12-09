17f287a8745c65f8b9aa20f4f75d28d7304b49af
ChapnesAI <job.chap@icloud.com>
2022-12-09 14:51:13 +0600

    - set up the connection to azure database

--- sql_investi/azure_sql_connect.py
+++ sql_investi/azure_sql_connect.py
@@ -1,1 +1,47 @@
-# host=stockschapnes.postgres.database.azure.com port=5432 dbname={your_database} user=chapnes password={your_password} sslmode=require
+import psycopg2
+
+host="stockschapnes.postgres.database.azure.com"
+port=5432
+dbname='postgres'
+user="chapnes"
+password="House333!"
+sslmode="require"
+
+
+
+connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=dbname,
+            sslmode=sslmode)
+
+connection.autocommit = True
+cursor = connection.cursor()
+
+def get_full_table():
+    with connection:
+        result = cursor.execute("""SElECT * FROM test_101;""")
+        return result, print(cursor.fetchall())
+
+
+
+def create_table():
+    with connection:
+        result = cursor.execute(
+            """CREATE TABLE test_102(
+                                    id serial PRIMARY KEY,
+                                    p18_05_20_01 numeric NOT NULL,
+                                    p19_05_20_06 numeric NOT NULL,
+                                    p19_10_21_01 numeric NOT NULL,
+                                    p20_05_21_10 numeric NOT NULL,
+                                    p21_06_22_04 numeric NOT NULL,
+                                    p22_01 numeric NOT NULL,
+                                    option varchar(200) NOT NULL);"""
+            )
+        return result, print("[INFO] Table created successfully")
+
+
+
+if __name__ == '__main__':
+    create_table()

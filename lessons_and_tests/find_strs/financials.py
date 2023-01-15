fe37bf0f703314d2d3f8ca5bd3695d3d2008c922
alexeychernysh <job.chap@icloud.com>
2023-01-16 00:02:56 +0600

    - update fun tests

--- lessons_and_tests/find_strs/financials.py
+++ lessons_and_tests/find_strs/financials.py
@@ -1,10 +1,17 @@
 import yfinance as yf
 import pandas as pd
 import numpy as np
+import fundamentalanalysis
 
 pd.set_option('display.max_rows', 1000)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
+
+
+from pandas_datareader import data as pdr
+
+
+
 
 
 tick = "MSFT"
@@ -14,5 +21,7 @@ column1 = 'Total Assets'
 
 
 df = ticker.balance_sheet.T
+
+pdr
 
 print(df)
